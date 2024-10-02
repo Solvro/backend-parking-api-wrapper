d8616a5941e6a379c54c304f6c8133f0362c5ca4
NimboStratus <playfulCloud@proton.me>
2024-10-02 21:33:35 +0200

    batman

---
+++ src/main/java/pl/wrapper/parking/ParkingApplication.java
@@ -0,0 +1,13 @@
+package pl.wrapper.parking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/wrapper/parking/ParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wrapper.parking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
