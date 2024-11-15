b133e68c7e78abddf4ca9d6c7aac67a5ec9a2012
Ignacy Smoliński <ignacy.smolinski@gmail.com>
2024-11-15 20:43:38 +0100

    comment

--- src/main/java/pl/wrapper/parking/facade/domain/ParkingServiceImpl.java
+++ src/main/java/pl/wrapper/parking/facade/domain/ParkingServiceImpl.java
@@ -3,8 +3,16 @@ package pl.wrapper.parking.facade.domain;
 import org.springframework.stereotype.Service;
 import pl.wrapper.parking.facade.ParkingService;
 import pl.wrapper.parking.pwrResponseHandler.PwrApiServerCaller;
+import pl.wrapper.parking.pwrResponseHandler.dto.ParkingResponse;
+
+import java.util.List;
 
 @Service
 record ParkingServiceImpl(PwrApiServerCaller pwrApiServerCaller) implements ParkingService {
-
+    //1. endpointy:
+    // - endpoint dla każdego parametru, zwraca jeden, kilka, lub nic
+    // - research, closest address
+    // - most free spots, everything that has => free spots, eveything has any free spots
+    // -* error handling, try to use as few exceptions as possible (think of a good way to do it)
+    //   dokumentacja + swagger do enpointa/endpointów i testy jednostkowe + test integracyjny dla waszego enpointa/enpoiontów
 }
