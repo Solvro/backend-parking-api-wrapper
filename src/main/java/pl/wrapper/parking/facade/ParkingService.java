0dc1b8aab58986a6d664111295a75c641b2e461c
Yan Samokar <ysamokar228@gmail.com>
2024-11-24 03:26:22 +0100

    feat: all parkings endpoint + closest parking stub

--- src/main/java/pl/wrapper/parking/facade/ParkingService.java
+++ src/main/java/pl/wrapper/parking/facade/ParkingService.java
@@ -1,3 +1,12 @@
 package pl.wrapper.parking.facade;
 
-public interface ParkingService {}
+import pl.wrapper.parking.pwrResponseHandler.dto.ParkingResponse;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+public interface ParkingService {
+    Mono<List<ParkingResponse>> getAllParkings();
+
+    Mono<ParkingResponse> getClosestParking(String address);
+}
--- src/main/java/pl/wrapper/parking/facade/domain/ParkingServiceImpl.java
+++ src/main/java/pl/wrapper/parking/facade/domain/ParkingServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 import pl.wrapper.parking.facade.ParkingService;
 import pl.wrapper.parking.pwrResponseHandler.PwrApiServerCaller;
 import pl.wrapper.parking.pwrResponseHandler.dto.ParkingResponse;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
@@ -15,4 +16,14 @@ record ParkingServiceImpl(PwrApiServerCaller pwrApiServerCaller) implements Park
     // - most free spots, everything that has => free spots, eveything has any free spots
     // -* error handling, try to use as few exceptions as possible (think of a good way to do it)
     //   dokumentacja + swagger do enpointa/endpointów i testy jednostkowe + test integracyjny dla waszego enpointa/enpoiontów
+
+    @Override
+    public Mono<List<ParkingResponse>> getAllParkings() {
+        return pwrApiServerCaller.fetchData();
+    }
+
+    @Override
+    public Mono<ParkingResponse> getClosestParking(String address) {
+        return Mono.empty();
+    }
 }
