0d2b2f1dfd448b86ed464d8da0e09c219ef1a61e
Yan Samokar <ysamokar228@gmail.com>
2024-11-24 03:26:22 +0100

    feat: all parkings endpoint + closest parking stub

--- src/main/java/pl/wrapper/parking/facade/domain/ParkingController.java
+++ src/main/java/pl/wrapper/parking/facade/domain/ParkingController.java
@@ -1,13 +1,33 @@
 package pl.wrapper.parking.facade.domain;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import pl.wrapper.parking.facade.ParkingService;
+import pl.wrapper.parking.pwrResponseHandler.dto.ParkingResponse;
+import reactor.core.publisher.Mono;
 
-@RequestMapping("v1")
+import java.util.List;
+
+@RequestMapping("/v1/parkings")
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 class ParkingController {
     private final ParkingService parkingService;
+
+    @GetMapping
+    public Mono<List<ParkingResponse>> getAllParkings() {
+        log.info("Fetching all parking lots.");
+        return parkingService.getAllParkings();
+    }
+
+    @GetMapping(params = "address")
+    public Mono<ParkingResponse> getClosestParking(@RequestParam("address") String address) {
+        log.info("Finding closest parking for address: {}", address);
+        return parkingService.getClosestParking(address);
+    }
 }
--- src/main/java/pl/wrapper/parking/facade/domain/ParkingServiceImpl.java
+++ src/main/java/pl/wrapper/parking/facade/domain/ParkingServiceImpl.java
@@ -3,14 +3,27 @@ package pl.wrapper.parking.facade.domain;
 import org.springframework.stereotype.Service;
 import pl.wrapper.parking.facade.ParkingService;
 import pl.wrapper.parking.pwrResponseHandler.PwrApiServerCaller;
+import pl.wrapper.parking.pwrResponseHandler.dto.ParkingResponse;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @Service
 record ParkingServiceImpl(PwrApiServerCaller pwrApiServerCaller) implements ParkingService {
-    // 1. endpointy:
+    //1. endpointy:
     // - endpoint dla każdego parametru, zwraca jeden, kilka, lub nic
     // - research, closest address
     // - most free spots, everything that has => free spots, eveything has any free spots
     // -* error handling, try to use as few exceptions as possible (think of a good way to do it)
-    //   dokumentacja + swagger do enpointa/endpointów i testy jednostkowe + test integracyjny dla waszego
-    // enpointa/enpoiontów
+    //   dokumentacja + swagger do enpointa/endpointów i testy jednostkowe + test integracyjny dla waszego enpointa/enpoiontów
+
+    @Override
+    public Mono<List<ParkingResponse>> getAllParkings() {
+        return pwrApiServerCaller.fetchData();
+    }
+
+    @Override
+    public Mono<ParkingResponse> getClosestParking(String address) {
+        return Mono.empty();
+    }
 }
