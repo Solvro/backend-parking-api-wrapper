dee7afb4dc24bf9d21f885fe2094fd68ee3bd4f2
Yan Samokar <ysamokar228@gmail.com>
2024-11-28 23:19:31 +0100

    fix: Nominatim API client config
    * add custom client exception
    * make "/search" endpoint asynchronous

--- src/main/java/pl/wrapper/parking/facade/config/NominatimClientConfig.java
+++ src/main/java/pl/wrapper/parking/facade/config/NominatimClientConfig.java
@@ -2,19 +2,26 @@ package pl.wrapper.parking.facade.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 import pl.wrapper.parking.facade.client.NominatimClient;
+import pl.wrapper.parking.facade.exception.NominatimClientException;
+import reactor.core.publisher.Mono;
 
 @Configuration
 public class NominatimClientConfig {
 
     @Bean
     public NominatimClient nominatimClient() {
-        WebClientAdapter webClientAdapter = WebClientAdapter.create(WebClient.create());
+        WebClient webClient = WebClient.builder()
+                .defaultStatusHandler(HttpStatusCode::isError,
+                        resp -> resp.bodyToMono(String.class)
+                                .flatMap(body -> Mono.error(new NominatimClientException(body))))
+                .build();
         HttpServiceProxyFactory factory = HttpServiceProxyFactory
-                .builderFor(webClientAdapter).build();
+                .builderFor(WebClientAdapter.create(webClient)).build();
 
         return factory.createClient(NominatimClient.class);
     }
