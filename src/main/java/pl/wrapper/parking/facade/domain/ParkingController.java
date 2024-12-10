560460ceaf48524f92828cf6a4c97e655c40a8cd
dominikkorwek <dominikkorwek@interia.pl>
2024-12-10 18:39:20 +0200

    init: add swagger for documentation, add documentation for parking controller

--- src/main/java/pl/wrapper/parking/facade/domain/ParkingController.java
+++ src/main/java/pl/wrapper/parking/facade/domain/ParkingController.java
@@ -2,6 +2,12 @@ package pl.wrapper.parking.facade.domain;
 
 import static pl.wrapper.parking.infrastructure.error.HandleResult.handleResult;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,46 +17,61 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import pl.wrapper.parking.facade.ParkingService;
+import pl.wrapper.parking.infrastructure.error.ErrorWrapper;
 import pl.wrapper.parking.infrastructure.error.Result;
 import pl.wrapper.parking.pwrResponseHandler.dto.ParkingResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
+@Tag(name = "Parking API")
 class ParkingController {
     private final ParkingService parkingService;
 
     @Value("${server.servlet.context-path}")
     private String apiPath;
 
+    @Operation(summary = "get parking by name if opened")
+    @ApiResponse(responseCode = "200", description = "parking found", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ParkingResponse.class)))
+    @ApiResponse(responseCode = "404", description = "parking not found",content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorWrapper.class)))
     @GetMapping("/name")
-    public ResponseEntity<String> getParkingByName(@RequestParam String name, @RequestParam(required = false) Boolean opened) {
+    public ResponseEntity<String> getParkingByName(@Parameter(description = "parking name") @RequestParam String name,@Parameter(description = "is parking opened") @RequestParam(required = false) Boolean opened) {
         log.info("Received request: get parking by name: {}", name);
         Result<ParkingResponse> result = parkingService.getByName(name, opened);
+
         return handleResult(result, HttpStatus.OK,  apiPath + "/name");
     }
 
+    @Operation(summary = "get parking by id if opened")
+    @ApiResponse(responseCode = "200", description = "parking found", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ParkingResponse.class)))
+    @ApiResponse(responseCode = "404", description = "parking not found",content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorWrapper.class)))
     @GetMapping("/id")
-    public ResponseEntity<String> getParkingById(@RequestParam Integer id, @RequestParam(required = false) Boolean opened) {
+    public ResponseEntity<String> getParkingById(@Parameter(description = "parking id") @RequestParam Integer id,@Parameter(description = "is parking opened") @RequestParam(required = false) Boolean opened) {
         log.info("Received request: get parking by id: {}", id);
         Result<ParkingResponse> result = parkingService.getById(id, opened);
         return handleResult(result, HttpStatus.OK, apiPath + "/id");
     }
 
+    @Operation(summary = "get parking by symbol if opened")
+    @ApiResponse(responseCode = "200", description = "parking found", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ParkingResponse.class)))
+    @ApiResponse(responseCode = "404", description = "parking not found",content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorWrapper.class)))
     @GetMapping("/symbol")
-    public ResponseEntity<String> getParkingBySymbol(@RequestParam String symbol, @RequestParam(required = false) Boolean opened) {
+    public ResponseEntity<String> getParkingBySymbol(@Parameter(description = "parking symbol") @RequestParam String symbol,@Parameter(description = "is parking opened") @RequestParam(required = false) Boolean opened) {
         log.info("Received request: get parking by symbol: {}", symbol);
         Result<ParkingResponse> result = parkingService.getBySymbol(symbol, opened);
         return handleResult(result, HttpStatus.OK,  apiPath + "/symbol");
     }
 
+    @Operation(summary = "get list of parkings by name, id, symbol and if opened")
+    @ApiResponse(responseCode = "200", description = "list with parkings", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = ParkingResponse.class))))
     @GetMapping
-    public ResponseEntity<List<ParkingResponse>> getParkingByParams(@RequestParam(required = false) String symbol,
-                                                     @RequestParam(required = false) Integer id,
-                                                     @RequestParam(required = false) String name,
-                                                     @RequestParam(required = false) Boolean opened) {
+    public ResponseEntity<List<ParkingResponse>> getParkingByParams(@Parameter(description = "parking symbol") @RequestParam(required = false) String symbol,
+                                                                    @Parameter(description = "parking id") @RequestParam(required = false) Integer id,
+                                                                    @Parameter(description = "parking name") @RequestParam(required = false) String name,
+                                                                    @Parameter(description = "is parking opened") @RequestParam(required = false) Boolean opened) {
         log.info("Received request: get parking by symbol: {},id: {} and name: {}",symbol,id,name);
         List<ParkingResponse> result = parkingService.getByParams(symbol, id, name, opened);
         return new ResponseEntity<>(result, HttpStatus.OK);
--- src/main/java/pl/wrapper/parking/infrastructure/error/ErrorWrapper.java
+++ src/main/java/pl/wrapper/parking/infrastructure/error/ErrorWrapper.java
@@ -1,5 +1,9 @@
 package pl.wrapper.parking.infrastructure.error;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.http.HttpStatus;
 
-public record ErrorWrapper(String errorMessage, HttpStatus expectedStatus, String Uri, HttpStatus occuredstatus) {}
+public record ErrorWrapper(@Schema(example = "An error has occured") String errorMessage,
+                           @Schema(example = "INTERNAL_SERVER_ERROR") HttpStatus expectedStatus,
+                           @Schema(example = "/v1/name") String Uri,
+                           @Schema(example = "INTERNAL_SERVER_ERROR") HttpStatus occuredstatus) {}
