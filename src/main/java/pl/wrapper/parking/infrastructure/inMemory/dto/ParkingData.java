b7cb36b70abde354396090c5cc29877e9af7c42c
Yan Samokar <ysamokar228@gmail.com>
2025-01-02 01:48:53 +0100

    feat: service's parking statistics unit testing

--- src/main/java/pl/wrapper/parking/infrastructure/inMemory/dto/ParkingData.java
+++ src/main/java/pl/wrapper/parking/infrastructure/inMemory/dto/ParkingData.java
@@ -2,7 +2,9 @@ package pl.wrapper.parking.infrastructure.inMemory.dto;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import lombok.Builder;
 
+@Builder
 public record ParkingData(int parkingId, int freeSpots, int totalSpots, LocalDateTime timestamp)
         implements Serializable {
     public record CompositeKey(int parkingId, LocalDateTime timestamp) implements Serializable {}
