cfdcc86519662a6c158fe568e5f99fa692f684a1
Mateusz Płaska <plaskamateuszi8@gmail.com>
2025-03-05 23:55:25 +0100

    feat: add interceptor to register requests

---
+++ src/main/java/pl/wrapper/parking/infrastructure/interceptor/ParkingRequestInterceptorConfig.java
@@ -0,0 +1,20 @@
+package pl.wrapper.parking.infrastructure.interceptor;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import pl.wrapper.parking.infrastructure.inMemory.ParkingRequestRepository;
+
+@Configuration
+@RequiredArgsConstructor
+public class ParkingRequestInterceptorConfig implements WebMvcConfigurer {
+    private final ParkingRequestRepository parkingRequestRepository;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new ParkingRequestInterceptor(parkingRequestRepository))
+                .addPathPatterns("/**")
+                .excludePathPatterns("/stats/**");
+    }
+}
